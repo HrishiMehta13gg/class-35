711f401faa7da04117dd70ea55a65424d3174fc7
HrishiMehta13gg <74819313+HrishiMehta13gg@users.noreply.github.com>
2021-02-23 16:45:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var ball;
+var databse;
+var position;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    var locofchild = database.ref("ball/position")
+    //ref()is used to refer to the location of the database
+ locofchild.on("value",readPosition,showError);
+ //. on() is used to create a listener which keeps on listening to the changes in the database
+    
+ ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+   database.ref("ball/position").set(
+       {
+           x:ball.x+x,
+           y:ball.y+y
+       }
+   )
+}
+function readPosition(data){
+    position=data.val();
+    console.log(position.x)
+    console.log(position.y)
+
+    ball.x=position.x
+    ball.y=position.y
+}
+function showError(){
+    console.log("Error in reading from the database")
+    
+}
